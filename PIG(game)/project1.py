95498e729d4ba693327c0b2eb5de6b5d97becdc2
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-06 17:57:59 -0400

    bug fixes

--- PIG(game)/project1.py
+++ PIG(game)/project1.py
@@ -7,10 +7,18 @@ def roll():#reusable function
 
     return roll
 while True :
-    players =input("Enter the number of players (1-4): ")
+    players =input("Enter the number of players (2-4): ")
     if players.isdigit():
         players =int(players)
         if 1 <= players <=4:
             
+            break
+        else:
+             print("Must be between 2-4 players.")
+    else:
+        print("Invalid, try again.")
+
+print(players)
+
 
 
