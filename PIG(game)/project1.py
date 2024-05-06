34a4f2066c0c4a790340dc81e29c9021d86fa919
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-06 17:51:52 -0400

    while true

--- PIG(game)/project1.py
+++ PIG(game)/project1.py
@@ -1,12 +1,16 @@
-import random
+import random #importing the random module
 
-def roll():
+def roll():#reusable function
     min_value =1
     max_value =6
     roll= random.randint(min_value,max_value)
 
     return roll
+while True :
+    players =input("Enter the number of players (1-4): ")
+    if players.isdigit():
+        players =int(players)
+        if 1 <= players <=4:
+            
 
 
-value= roll()
-print (value)
