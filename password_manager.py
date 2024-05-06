2340c2017902d713b52075c9e5e36960287539aa
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-06 03:43:54 -0400

    def add()

--- password_manager.py
+++ password_manager.py
@@ -1,12 +1,19 @@
-pwd= input("What is the master password? ")
+master_pwd= input("What is the master password? ")
 
 # function is an executable reusable line of code
 def view():
     pass
 
 def add():
-    pass
+    name= input ("Account Name: ")
+    pwd = input ("Password: ")
+# with automatically closes files for us better than file open wich requieres file.close()
+# modes:w- overwrite, r-read a-append mode(adds or created new file )
+# f is our file name
+    with open ("password.txt", 'a') as f:
+        f.write(name+""+pwd)
 
+  
 while True:
     mode= input("Would you like to add a new password or view existing ones.Type view or add.Press Q to quit").lower()
     if mode == "q":
