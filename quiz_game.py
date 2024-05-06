2f7e20d8c55248699c00cf5792c00d1f9ec69012
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-05 20:22:30 -0400

    add

---
+++ quiz_game.py
@@ -0,0 +1,13 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes" :
+    quit()
+
+print("Okay!Lets play dear ")
+
+answer = input("What does CPU stand for ?")
+
+
+
