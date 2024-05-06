12191b7ccb423adb2097d3b7e139d54802e314ff
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-05 22:49:01 -0400

    random number correct end program

--- number_guesser.py
+++ number_guesser.py
@@ -17,4 +17,23 @@ else:
 
 random_number = random.randint(0,top_of_range)
 
-print(random_number)
+while True:
+   user_guess =input("Make a guess: ")
+   if user_guess.isdigit() :
+    user_guess= int(user_guess)
+   else:
+      print("Print a number next time.")
+      continue
+   if user_guess == random_number:
+      print("You got it !")
+      break
+   else:
+      print("You got it wrong!")
+      
+   
+
+
+
+    
+
+ 
