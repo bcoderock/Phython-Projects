0e1865f20a2c02b9324e56bec529b9013a47508a
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-06 19:12:58 -0400

    complete

--- PIG(game)/project1.py
+++ PIG(game)/project1.py
@@ -1,3 +1,5 @@
+#PIG game
+
 import random #importing the random module
 
 def roll():#reusable function
@@ -24,23 +26,31 @@ player_scores = [ 0 for _ in range(players)] #list comprehention
 
 while max(player_scores) < max_score :
 
-    for i in range(players):
-
+    for player_idx in range(players):
+        print("\n Player number", player_idx + 1, "turn has started!")
+        print("Your total score is:",player_scores[player_idx],"\n")
         current_score =0 
 
-        should_roll =input("Would you like to roll (y)?")
-        if should_roll.lower() != "y" :
-            break
-        value =roll()
-        if value ==1 :
-            print("You rolled a 1 .Turn done!")
-            break
-        else:
-            current_score +=value
-            print("You rolled a:",value)
-            
-        print("Your score is:",current_score)
-    
+        while True:
+
+            should_roll =input("Would you like to roll (y)?")
+            if should_roll.lower() != "y" :
+                break
+            value =roll()
+            if value ==1 :
+                print("You rolled a 1 .Turn done!")
+                current_score=0
+                break
+            else:
+                current_score +=value
+                print("You rolled a:",value)
+                
+            print("Your score is:",current_score)
+        
+        player_scores[player_idx] += current_score
+        print("Your total score is :",player_scores[player_idx])
 
 
-
+max_score =max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number",winning_idx + 1, "is the winner with a score of:",max_score)# this make sure our starting number is above 1
