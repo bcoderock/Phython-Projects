0cd154523c7bb8dcf53fb50fb25c4d43e041ccd6
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-06 02:44:18 -0400

    complete

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,13 +1,13 @@
-name =input("Type your name : ")
-print("Welcome", name, "to this adventure!")
+user_name =input("Type your name : ")
+print("Welcome", user_name, "to this adventure!")
 
 answer= input ("You have come to a dirt road , it has come to an end.You can only go left and right.Which way would you like to go? ").lower()
 
 if answer =="left":
-  answer =input ("You have come to a river.You can walk around it or swim to swim across:Type walk or swim").lower()
+  answer =input ("You have come to a river.You can walk around it or swim to swim across:Type walk or swim:").lower()
 
   if answer == "swim":
-    print("Hahahaha.You swam across and were esten by crocodiles")
+    print("Hahahaha.You swam across and were eaten by crocodiles")
 
   elif answer == "walk" :
     print("You walked for many miles,ran out of water.You lost !")
@@ -18,13 +18,13 @@ if answer =="left":
 
  
 elif answer == "right":
- answer= input("You come to a bridge, it looks wobbly,do you want to cross it or head back.Type cross or back").lower()  
+ answer= input("You come to a bridge, it looks wobbly,do you want to cross it or head back.Type cross or back: ").lower()  
 
  if answer == "cross":
 
-    answer =input (" Phew ! You cross the bridge and meet a stranger.Do you talk to them?.Type yes or no").lower()
+    answer =input (" Phew ! You cross the bridge and meet a stranger.Do you talk to them?.Type yes or no: ").lower()
     if answer == "yes" :
-      print("You talk to the stranger and they give you a ride out of the forest.YOU SURVIVE").lower()
+      print("You talk to the stranger and they give you a ride out of the forest.YOU SURVIVE")
     elif answer == "no":
       print("You ignore the stranger and walk away. Some few minutes later you are attacked by a lion.YOU DIE")
       
@@ -42,3 +42,6 @@ elif answer == "right":
 
 else:
   print("Not a valid option.You lose.")
+
+
+print("Thanks for playing", user_name )
