5f30df0b61712b9c3d4822e222e391c7ac0c85da
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-06 18:46:12 -0400

    add while loop

--- PIG(game)/project1.py
+++ PIG(game)/project1.py
@@ -23,10 +23,23 @@ max_score =50
 player_scores = [ 0 for _ in range(players)] #list comprehention
 
 while max(player_scores) < max_score :
-    should_roll =input("Would you like to roll (y)?")
-    if should_roll.lower() == "y" :
-       break
-    value =roll()
+
+    for i in range(players):
+
+        current_score =0 
+
+        should_roll =input("Would you like to roll (y)?")
+        if should_roll.lower() != "y" :
+            break
+        value =roll()
+        if value ==1 :
+            print("You rolled a 1 .Turn done!")
+            break
+        else:
+            current_score +=value
+            print("You rolled a:",value)
+            
+        print("Your score is:",current_score)
     
 
 
